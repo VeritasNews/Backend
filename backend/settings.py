ccef638a1bc5ce518d015ae8afb8e3ef5c3659ef
zedyjy <doga.dellal@ug.bilkent.edu.tr>
2025-05-09 13:04:05 +0300

    fix: update DATABASE_URL and refactor database settings to use dj_database_url

--- backend/settings.py
+++ backend/settings.py
@@ -134,20 +134,8 @@ import dj_database_url
 import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'veritas',
-        'USER': 'postgres.pxjomdphjbkrjnihjekl',
-        'PASSWORD': 'Veritasnews123',
-        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
-        'PORT': '6543',
-        'OPTIONS': {
-            'sslmode': 'require',
-            'client_encoding': 'UTF8',
-        },
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 
 
 # Password validation
